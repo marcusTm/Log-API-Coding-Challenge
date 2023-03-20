9d9e294051ed527ed7cd8987a5b8342d9dc2daee
marcusTm <marcusmathiesen@yahoo.dk>
2023-03-20 19:38:54 +0100

    Used spring initializer to create project

---
+++ logapi/src/main/java/com/maersk/logapi/LogapiApplication.java
@@ -0,0 +1,13 @@
+package com.maersk.logapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogapiApplication.class, args);
+	}
+
+}
---
+++ logapi/src/test/java/com/maersk/logapi/LogapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maersk.logapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
